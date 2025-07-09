a7f75fdbfc49aaee547be0d070b1ea00a230dd45
xanna6 <apiotrow96@gmail.com>
2025-07-09 19:52:38 +0200

    spring boot project created

---
+++ backend/MediFlow/src/main/java/com/apiot/mediflow/MediFlowApplication.java
@@ -0,0 +1,13 @@
+package com.apiot.mediflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediFlowApplication.class, args);
+    }
+
+}
---
+++ backend/MediFlow/src/test/java/com/apiot/mediflow/MediFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiot.mediflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
