7bb3ff8a945dcbb53f10b055f5049336a78846a6
xanna6 <apiotrow96@gmail.com>
2026-02-08 18:08:05 +0100

    displaying tabs depending on the user's role

--- frontend/MediFlow/src/components/SubNavbar.jsx
+++ frontend/MediFlow/src/components/SubNavbar.jsx
@@ -1,22 +1,66 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../auth/AuthContext";
 
 export default function SubNavbar() {
+    const { user, hasRole } = useAuth();
+
+        if (!user) {
+        return (
+            <aside className="subnavbar">
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink to="/" end className={({ isActive }) => isActive ? "active" : ""}>
+                                Strona główna
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/make-appointment"
+                                     className={({ isActive }) => isActive ? "active" : ""}>
+                                Umów wizytę
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/results"
+                                     className={({ isActive }) => isActive ? "active" : ""}>
+                                Wyniki
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+        );
+    }
+
     return (
         <aside className="subnavbar">
             <nav>
                 <ul>
-                    <li><NavLink to="/" end className={({isActive}) => isActive ? "active" : ""}>Strona główna</NavLink>
-                    </li>
-                    <li><NavLink to="/referrals"
-                                 className={({isActive}) => isActive ? "active" : ""}>Skierowania</NavLink></li>
-                    <li><NavLink to="/make-appointment" className={({isActive}) => isActive ? "active" : ""}>Umów
-                        wizytę</NavLink></li>
-                    <li><NavLink to="/appointments"
-                                 className={({isActive}) => isActive ? "active" : ""}>Wizyty</NavLink></li>
-                    <li><NavLink to="/samples"
-                                 className={({isActive}) => isActive ? "active" : ""}>Próbki</NavLink></li>
-                    <li><NavLink to="/results" className={({isActive}) => isActive ? "active" : ""}>Wyniki</NavLink>
-                    </li>
+                    {hasRole("ROLE_DOCTOR") && (
+                        <li>
+                            <NavLink to="/referrals"
+                                     className={({ isActive }) => isActive ? "active" : ""}>
+                                Skierowania
+                            </NavLink>
+                        </li>
+                    )}
+
+                    {hasRole("ROLE_LAB") && (
+                        <>
+                            <li>
+                                <NavLink to="/appointments"
+                                         className={({ isActive }) => isActive ? "active" : ""}>
+                                    Wizyty
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/samples"
+                                         className={({ isActive }) => isActive ? "active" : ""}>
+                                    Próbki
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </nav>
         </aside>
