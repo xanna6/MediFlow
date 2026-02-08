c3ab2069cab0ac2b713447847a0f7bb88324ad58
xanna6 <apiotrow96@gmail.com>
2026-02-08 17:57:07 +0100

    fix to return user role after login

--- backend/MediFlow/src/main/java/com/apiot/mediflow/auth/JwtService.java
+++ backend/MediFlow/src/main/java/com/apiot/mediflow/auth/JwtService.java
@@ -1,18 +1,19 @@
 package com.apiot.mediflow.auth;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -34,11 +35,16 @@ public class JwtService {
     }
 
     public String generateToken(UserDetails userDetails) {
-        return generateToken(new HashMap<>(), userDetails);
-    }
 
-    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return buildToken(extraClaims, userDetails, jwtExpiration);
+        List<String> roles = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", roles);
+
+        return buildToken(claims, userDetails, jwtExpiration);
     }
 
     public long getExpirationTime() {
